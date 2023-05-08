18b31d9326ac11b4a98469de703538e870140151
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-08 23:00:51 +0300

    add cards listing

--- src/pages/ListingPage.vue
+++ src/pages/ListingPage.vue
@@ -1,5 +1,37 @@
+<script>
+import CardItem from "../components/CardItem.vue";
+
+export default {
+  name: "my-component",
+  data() {
+    return {
+      cards: [
+        { img: "link", price: "100", description: "" },
+        { img: "link", price: "100", description: "" },
+        { img: "link", price: "100", description: "" },
+        { img: "link", price: "100", description: "" },
+        
+      ],
+    };
+  },
+  components: { CardItem },
+};
+</script>
 <template>
-    <div>
-        listing
-    </div>
+  <ul id="cardsProducts">
+    <li v-for="card in cards">
+      <CardItem></CardItem>
+    </li>
+  </ul>
 </template>
+<style lang="scss">
+#cardsProducts {
+  display: flex;
+  flex-wrap: wrap;
+  
+  li {
+    list-style: none;
+    margin: 1em;
+  }
+}
+</style>
