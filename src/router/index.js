17d30bcb7d3dc30114a750f6f0e219ff0f68fc14
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-08 20:08:43 +0300

    s

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HelloWorld from '../components/HelloWorld.vue'
-
-
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HelloWorld
+      component: 
     }
 
     
