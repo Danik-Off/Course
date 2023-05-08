4fab41de0b5906ab4615a747d7b5aeb1db66a719
Anton Toneyev <aatoneyev@gmail.com>
2023-05-08 23:00:44 +0300

    aa

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -7,17 +7,21 @@
 					<span class="product_support_text">Жилет с нашивкой компасом</span>
 				</div>
 				<span class="product_description">
-					Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет.
+					Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот
+					жилет.
 				</span>
 				<span class="product_price">
 					30 000 руб
 				</span>
 			</div>
-			<div class="product_dropdownSize">
-				<span>Выберите размер</span>
-				<label></label>
+			<div class="product_action">
+				<div class="product_dropdownSize">
+					<span>Выберите размер</span>
+					<label></label>
+				</div>
+				<button class="product_addToCart"><span>Добавить в корзину</span></button>
 			</div>
-			<button class="product_addToCart"><span>Добавить в корзину</span></button>
+
 		</div>
 		<div class="product_gallery">
 			<div class="product_gallery_photos">
@@ -39,63 +43,102 @@
 </template>
 
 <style lang="scss">
-	.product {
-		background: #FFFFFF;
+.product {
+	background: #FFFFFF;
 
-		.product_content{
-
-
-			.product_header{
+	.product_content {
 
 
-				.product_heading{
+		.product_header {
 
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			padding: 0px;
+			gap: 40px;
 
-					.product_name{
-						color:rgba(53.00000064074993, 65.0000037252903, 83.00000265240669, 1);
-						width:288px;
-						height:40px;
-						font-family:Inter;
-						text-align:left;
-						font-size:32px;
-						letter-spacing:0;
-						
-					}
-					.product_support_text{
-						color: rgba(102.00000151991844, 112.000000923872, 133.00000727176666, 1);
-    					width: 288px;
-    					height: 30px;
-    					left: 0px;
-    					top: 40px;
-    					font-family: Inter;
-    					text-align: left;
-    					font-size: 20px;
-					}
+			flex: none;
+			order: 0;
+			flex-grow: 0;
+
+			.product_heading {
+
+				display: flex;
+				flex-direction: column;
+				align-items: flex-start;
+				padding: 0px;
+
+				width: 288px;
+				height: 70px;
+				flex: none;
+				order: 0;
+				flex-grow: 0;
+
+				.product_name {
+					color: rgba(53.00000064074993, 65.0000037252903, 83.00000265240669, 1);
+					width: 288px;
+					height: 40px;
+					font-family: Inter;
+					text-align: left;
+					font-size: 32px;
+					letter-spacing: 0;
+
 				}
-				
-				.product_description{
+
+				.product_support_text {
 					color: rgba(102.00000151991844, 112.000000923872, 133.00000727176666, 1);
-    				width: 648px;
-					height: 60px;
-					top: 110px;
+					width: 288px;
+					height: 30px;
+					left: 0px;
+					top: 40px;
 					font-family: Inter;
 					text-align: left;
 					font-size: 20px;
 				}
-
-				.product_price{
-					color: rgba(13.174998834729195, 19.762502051889896, 32.93749898672104, 1);
-					width: 648px;
-					height: 48px;
-					top: 210px;
-					font-family: Inter;
-					text-align: left;
-					font-size: 36px;
-				}
 			}
 
-			.product_dropdownSize{
-				display:flex;
+			.product_description {
+				color: rgba(102.00000151991844, 112.000000923872, 133.00000727176666, 1);
+				width: 648px;
+				height: 60px;
+				top: 110px;
+				font-family: Inter;
+				text-align: left;
+				font-size: 20px;
+			}
+
+			.product_price {
+				color: rgba(13.174998834729195, 19.762502051889896, 32.93749898672104, 1);
+				width: 648px;
+				height: 48px;
+				top: 210px;
+				font-family: Inter;
+				text-align: left;
+				font-size: 36px;
+			}
+		}
+
+		.product_action {
+
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			padding: 0px;
+			gap: 16px;
+
+			flex: none;
+			order: 1;
+			flex-grow: 0;
+
+			.product_dropdownSize {
+
+				box-sizing: border-box;
+				display: flex;
+				flex-direction: row;
+				justify-content: center;
+				align-items: center;
+				padding: 12px 16px;
+				gap: 8px;
 				border: 1px solid #EAECF0;
 				border-radius: 8px;
 				background-color: rgba(255, 255, 255, 1);
@@ -108,22 +151,35 @@
 				align-items: center;
 				justify-content: center;
 
-				span{
+				flex: none;
+				order: 0;
+				align-self: stretch;
+				flex-grow: 0;
+
+				span {
 					color: rgba(53.00000064074993, 65.0000037252903, 83.00000265240669, 1);
-					width: 144px;
-					height: 24px;
+					
+
+					font-family: 'Inter';
+					font-style: normal;
+					font-weight: 600;
+					font-size: 16px;
+					line-height: 24px;
 					
 					font-family: Inter;
-					text-align: start;
+					text-align: left;
 					font-size: 16px;
 				}
 
-				label{
+				label {}
 
-				}
+
+
+
 			}
 
-			.product_addToCart{
+			.product_addToCart {
+
 				background-color: rgba(13.174998834729195, 19.762502051889896, 32.93749898672104, 1);
 				width: 204px;
 				height: 48px;
@@ -133,7 +189,7 @@
 				border-bottom-left-radius: 8px;
 				border-bottom-right-radius: 8px;
 
-				span{
+				span {
 					color: rgba(255, 255, 255, 1);
 					width: 160px;
 					height: 24px;
@@ -144,6 +200,13 @@
 					font-size: 16px;
 				}
 			}
+
+
 		}
+
+
+
+
 	}
+}
 </style>
