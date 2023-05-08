969df8dc084ed99d4cc5bf63186f0478907b802e
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-08 19:59:40 +0300

    ol

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="back"></div>
+
+    <nav>
+      <ul class="menu">
+        <hr />
+        <RouterLink to="/">Главная</RouterLink>
+        <RouterLink to="/speakers">Корзина</RouterLink>
+        <RouterLink to="/:id"></RouterLink>
+        <hr />
+        <button class="videoControlButtton"></button>
+      </ul>
+    </nav>
+  </header>
+</template>
