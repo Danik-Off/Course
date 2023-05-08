0b47e184a5314bf65b5bce934338cfbcecd13d08
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-08 21:15:00 +0300

    sda

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,62 @@
 <script>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from "vue-router";
 </script>
 <template>
   <header>
-    <div class="back"></div>
     <nav>
-      <ul class="menu">
-        <hr />
-        <RouterLink to="/">Главная</RouterLink>
-        <RouterLink to="/speakers">Корзина</RouterLink>
-        <RouterLink to="/:id"></RouterLink>
-        <hr />
-        <button class="videoControlButtton"></button>
-      </ul>
+      <RouterLink   to="/"  ><img src="/Vector.svg"/><span>Курсовой проект</span></RouterLink>
+      <div >
+        <RouterLink   to="/Login"><img src="../assets/login.svg"/></RouterLink>
+        <RouterLink   to="/:id"><img src="../assets/cart.svg"/></RouterLink>
+      </div>
     </nav>
   </header>
 </template>
+<style lang="scss">
+nav{
+  border-bottom: 1px solid black;
+  text-decoration: none;
+  display: flex;
+     justify-content: space-between;
+     align-items: center;
+     flex-wrap: wrap; 
+     align-content: space-between;
+  padding: 1em;
+
+  div{
+    display: flex;
+    a{
+      display: flex;
+    text-decoration: none;
+    height: 100%;
+    padding: 0;
+    margin: 10px;
+    align-items: center;
+    }
+  }
+  a{
+    display: flex;
+    text-decoration: none;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    align-items: center;
+    img{
+      display: flex;
+      object-fit: contain;
+    }
+    span{
+      display: flex;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  text-align: center;
+  color: #354153;
+  text-decoration: none;
+    }
+  }
+  
+ 
+}
+
+</style>
