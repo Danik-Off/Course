a2247e7d9f69dc5e77630177209abc43be24a287
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-08 20:23:46 +0300

    sada

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import {RouterView } from 'vue-router';
-
+import Header from './components/header.vue'
 </script>
 
 <template>
-
+  <Header></Header>
   <div class="main">
     <RouterView />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import ListingPage from '../pages/ListingPage.vue'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: 
+      name: 'listing',
+      component: ListingPage
     }
 
     
