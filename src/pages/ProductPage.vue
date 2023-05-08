63bd1178c57681548c20a9cd12b81cdf25930b59
Anton Toneyev <aatoneyev@gmail.com>
2023-05-08 22:42:01 +0300

    b

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -14,9 +14,10 @@
 				</span>
 			</div>
 			<div class="product_dropdownSize">
-				Выберите размер
+				<span>Выберите размер</span>
+				<label></label>
 			</div>
-			<button class="product_addToCart">Добавить в корзину</button>
+			<button class="product_addToCart"><span>Добавить в корзину</span></button>
 		</div>
 		<div class="product_gallery">
 			<div class="product_gallery_photos">
@@ -70,6 +71,77 @@
     					text-align: left;
     					font-size: 20px;
 					}
+				}
+				
+				.product_description{
+					color: rgba(102.00000151991844, 112.000000923872, 133.00000727176666, 1);
+    				width: 648px;
+					height: 60px;
+					top: 110px;
+					font-family: Inter;
+					text-align: left;
+					font-size: 20px;
+				}
+
+				.product_price{
+					color: rgba(13.174998834729195, 19.762502051889896, 32.93749898672104, 1);
+					width: 648px;
+					height: 48px;
+					top: 210px;
+					font-family: Inter;
+					text-align: left;
+					font-size: 36px;
+				}
+			}
+
+			.product_dropdownSize{
+				display:flex;
+				border: 1px solid #EAECF0;
+				border-radius: 8px;
+				background-color: rgba(255, 255, 255, 1);
+				width: 204px;
+				height: 48px;
+				border-top-left-radius: 8px;
+				border-top-right-radius: 8px;
+				border-bottom-left-radius: 8px;
+				border-bottom-right-radius: 8px;
+				align-items: center;
+				justify-content: center;
+
+				span{
+					color: rgba(53.00000064074993, 65.0000037252903, 83.00000265240669, 1);
+					width: 144px;
+					height: 24px;
+					
+					font-family: Inter;
+					text-align: start;
+					font-size: 16px;
+				}
+
+				label{
+
+				}
+			}
+
+			.product_addToCart{
+				background-color: rgba(13.174998834729195, 19.762502051889896, 32.93749898672104, 1);
+				width: 204px;
+				height: 48px;
+				top: 64px;
+				border-top-left-radius: 8px;
+				border-top-right-radius: 8px;
+				border-bottom-left-radius: 8px;
+				border-bottom-right-radius: 8px;
+
+				span{
+					color: rgba(255, 255, 255, 1);
+					width: 160px;
+					height: 24px;
+					left: 22px;
+					top: 12px;
+					font-family: Inter;
+					text-align: left;
+					font-size: 16px;
 				}
 			}
 		}
