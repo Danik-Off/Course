8f0fa5b54a4066bda6271d6c2291e5afd8ac01ea
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-08 18:35:53 +0300

    s

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import cardItem from './components/cardItem.vue';
-
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
-<template>   
- <cardItem></cardItem>
-<ul id="example-1">
- 
-  <li v-for="item in items" :key="item.message">
-    {{ item.message }}
-  </li>
-</ul>
-  
-
+<template>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-
-
-
+<style scoped>
+</style>
