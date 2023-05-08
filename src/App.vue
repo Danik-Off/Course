60876adb7705eb83d5ad13f1cc9cdb90a396518b
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-08 18:19:19 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import cardItem from './components/cardItem.vue';
+
+</script>
+
+<template>   
+ <cardItem></cardItem>
+<ul id="example-1">
+ 
+  <li v-for="item in items" :key="item.message">
+    {{ item.message }}
+  </li>
+</ul>
+  
+
+</template>
+
+
+
+
---
+++ src/components/CardItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="cardItem">
+    <img src="images/image_1.png" />
+    <span class="price">30 000 руб</span>
+    <div class="e112_2409">
+      <div class="e112_2410">
+        <span class="e112_2411">Бренд</span
+        ><span class="e112_2412">Каменный остров</span>
+      </div>
+      <span class="e112_2413">Жилет с нашивкой компасом </span>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.cardItem {
+  background-color: rgba(255, 255, 255, 1);
+  color: black;
+  width: 20em;
+  height:30em;
+  img
+  {
+    object-fit: contain;
+    height:100%;
+    width: 100%;
+  }
+}
+</style>
