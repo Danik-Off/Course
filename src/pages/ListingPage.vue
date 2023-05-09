19204f70a1d88fa8a0e2b2ff6751b274d551b9c7
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-09 19:32:45 +0300

    т

--- src/pages/ListingPage.vue
+++ src/pages/ListingPage.vue
@@ -2,11 +2,14 @@
 import CardItem from "../components/CardItem.vue";
 
 export default {
+
+ 
   name: "my-component",
   data() {
     return {
       cards: [
         {
+          id:21972830,
           imgUrl: "images/image_1.png",
           brand: "Каменный остров",
           price: "30000",
@@ -14,53 +17,34 @@ export default {
             "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
           specification: "Жилет с нашивкой компасом ",
         },
-        {
-          imgUrl: "images/image_1.png",
-          brand: "Каменный остров",
-          price: "30000",
-          description:
-            "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
-          specification: "Жилет с нашивкой компасом ",
-        },
-        {
-          imgUrl: "images/image_1.png",
-          brand: "Каменный остров",
-          price: "30000",
-          description:
-            "Инновации и функциональность нnnnnnnnnnnnnnnnnnnn nnnnnnnn nnnn nnn nnnnnnn nnnnnnnnnn nnnnnnnnnn  \n\rаходятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
-          specification: "Жилет с нашивкой компасом ",
-        },
-        {
-          imgUrl: "images/image_1.png",
-          brand: "Каменный остров",
-          price: "30000",
-          description:
-            "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
-          specification: "Жилет с нашивкой компасом ",
-        },
-        {
-          imgUrl: "images/image_1.png",
-          brand: "Каменный остров",
-          price: "30000",
-          description:
-            "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
-          specification: "Жилет с нашивкой компасом ",
-        },
+       
       ],
     };
   },
   components: { CardItem },
+  methods: {
+        openCardProduct(e)
+        {
+          const productCard = e.currentTarget;
+          console.log("open product id:"+productCard.id);
+            
+        }
+    }
+
+
 };
 </script>
 <template>
   <ul id="cardsProducts">
     <li v-for="card in cards">
       <CardItem
+        :id="card.id"
         :imgUrl="card.imgUrl"
         :brand="card.brand"
         :specification="card.specification"
         :price="card.price"
         :description="card.description"
+        :onClick="openCardProduct"
       ></CardItem>
     </li>
   </ul>
