7544f4d71b19a9733d242b7edede0783015e99ce
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-09 20:30:14 +0300

    fghjkl;

--- src/Api.js
+++ src/Api.js
@@ -1,31 +1,60 @@
-class Api
-{
-    login()
-    {
-
+export default function Api() {
+    this.cart=[];
+    this.data =  [
+        {
+          id:21972830,
+          imgUrl: "images/image_1.png",
+          brand: "Каменный остров",
+          price: "30000",
+          description:
+            "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
+          specification: "Жилет с нашивкой компасом ",
+        },
+        {
+            id:21972812,
+            imgUrl: "images/image_1.png",
+            brand: "Каменный остров",
+            price: "30000",
+            description:
+              "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
+            specification: "Жилет с нашивкой компасом ",
+          }, {
+            id:21972844,
+            imgUrl: "images/image_1.png",
+            brand: "Каменный остров",
+            price: "30000",
+            description:
+              "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
+            specification: "Жилет с нашивкой компасом ",
+          }, {
+            id:219728152,
+            imgUrl: "images/image_1.png",
+            brand: "Каменный остров",
+            price: "30000",
+            description:
+              "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
+            specification: "Жилет с нашивкой компасом ",
+          }, {
+            id:219727660,
+            imgUrl: "images/image_1.png",
+            brand: "Каменный остров",
+            price: "30000",
+            description:
+              "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
+            specification: "Жилет с нашивкой компасом ",
+          },
+       
+      ];
+    this.getById = (id)=>{
+      return this.data.filter(item=>item.id==id);
     }
-    getProducts()
+    this.get = ()=>
     {
-
+        return this.data ;
     }
-    getProduct()
-    {
-
-    }
-    getCart()
-    {
-
-    }
-    addToCart()
-    {
-
-    }
-    deleteFromCart()
-    {
-        
-    }
-
+    this.getCart = ()=>{}
+    this.addToCart = ()=>{}
+    this.addToCart = ()=>{}
+    this.login = ()=>{}
 
 }
-const api = new Api;
-api.login()
--- src/pages/ListingPage.vue
+++ src/pages/ListingPage.vue
@@ -1,28 +1,23 @@
 <script>
 import CardItem from "../components/CardItem.vue";
-
+import Api from "../Api.js"
 export default {
 
  
   name: "my-component",
   data() {
     return {
-      cards: [
-        {
-          id:21972830,
-          imgUrl: "images/image_1.png",
-          brand: "Каменный остров",
-          price: "30000",
-          description:
-            "Инновации и функциональность находятся в приоритете у Каменного острова, о чем свидетельствует этот жилет",
-          specification: "Жилет с нашивкой компасом ",
-        },
-       
-      ],
+      cards:[]
     };
   },
+  beforeMount(){console.log("test");this.loadCards()},
   components: { CardItem },
   methods: {
+        loadCards()
+        {
+         const api = new Api();
+         this.cards = api.get();
+        },
         openCardProduct(e)
         {
           const productCard = e.currentTarget;
