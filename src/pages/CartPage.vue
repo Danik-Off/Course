6f67ca217dccbaf05c7596597ca42ad403e52e09
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-11 22:16:05 +0300

    modified:   src/pages/CartPage.vue

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,77 +1,63 @@
 <script>
-import CardItem from "../components/CardItem.vue";
-import CartItem from "../components/CartItem.vue";
+import CardItem from '../components/CardItem.vue';
 import Api from "../Api";
 import router from "../router";
 export default {
-  data() {
+
+    data() {
     return {
-      cards: [],
-      cartProducts:[]
+      cards:[]
     };
   },
-
-  beforeMount() {
-    console.log("test");
-    this.loadCards();
- 
-  },
-  components: { CardItem,CartItem },
+  
+  beforeMount(){console.log("test");this.loadCards()},
+  components: { CardItem },
   methods: {
-    loadCards() {
-      const api = new Api();
-      this.cards = api.get(4);
-      this.cartProducts = api.getCart();
-      console.log(this.cartProducts)
-    },
-    openCardProduct(e) {
-      const productCard = e.currentTarget;
-      console.log("open product id:" + productCard.id);
-      router.push({ name: "product", params: { id: productCard.id } });
-    },
-  },
-};
+        loadCards()
+        {
+         const api = new Api();
+         this.cards = api.get(4);
+        },
+        openCardProduct(e)
+        {
+          const productCard = e.currentTarget;
+          console.log("open product id:"+productCard.id);
+          router.push({ name: 'product', params: { id: productCard.id } })
+        }
+    }
+
+
+}
 </script>
 
 <template>
   <h1>Корзина</h1>
-  <div v-if="cartProducts.length>0">
-    <hr />
+  <div >
+    <hr/>
     Ваша корзина пуста
-    <hr />
+    <hr/>
     <div>
-      <h1>Но у нас есть персональные предложения для вас</h1>
-      <ul id="cardsProducts">
-        <li v-for="card in cards">
-          <CardItem
-            :id="card.id"
-            :imgUrl="card.imgUrl"
-            :brand="card.brand"
-            :specification="card.specification"
-            :price="card.price"
-            :description="card.description"
-            :onClick="openCardProduct"
-          ></CardItem>
-        </li>
-      </ul>
+        <h1>Но у нас есть персональные предложения для вас</h1>
+        <ul id="cardsProducts">
+            <li v-for="card in cards">
+              <CardItem
+                :id="card.id"
+                :imgUrl="card.imgUrl"
+                :brand="card.brand"
+                :specification="card.specification"
+                :price="card.price"
+                :description="card.description"
+                :onClick="openCardProduct"
+              ></CardItem>
+            </li>
+          </ul>
     </div>
   </div>
-  <div v-else>
-    <ul id="cardsProducts">
-        <li v-for="card in cards">
-          <CartItem
-            :id="card.id"
-            :imgUrl="card.imgUrl"
-            :brand="card.brand"
-            :specification="card.specification"
-            :price="card.price"
-            :description="card.description"
-            :onClick="openCardProduct"
-          ></CartItem>
-        </li>
-      </ul>
+  <div>
 
   </div>
-</template>
+  </template>
+ 
+  <style lang="scss" >
 
-<style lang="scss"></style>
+  </style>
