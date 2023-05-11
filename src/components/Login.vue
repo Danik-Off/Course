1dbc9c780b1cd66a099e7ea3ea0933b5784883ad
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-11 18:48:43 +0300

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="login">
-        <div></div>
+        <div class="back"></div>
     </div>
 </template>
-<script lang="scss">
+<style lang="scss">
 .login {
+    display:block;
+    .back{
+        width:100%;
+        height: 100%;
+        background: #0D1421;
+        opacity: 0.4;
+    }
     
 }
-</script>
+</style>
